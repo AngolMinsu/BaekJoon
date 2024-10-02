36ec30056ed0c8a929b8312ae323c33ed28e41e9
AngolMinsu <steinsgate888@gmail.com>
2024-10-02 11:11:52 +0900

    백준 2229 시작

---
+++ src/backjoon/M2229.java
@@ -0,0 +1,11 @@
+package backjoon;
+
+import java.io.*;
+
+public class M2229 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+    }
+}
