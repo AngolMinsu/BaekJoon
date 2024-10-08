a64c833776dbe150862dee03639d491a2e2118d6
AngolMinsu <steinsgate888@gmail.com>
2024-10-08 16:25:44 +0900

    백준 13343 해결 다사다난

--- src/backjoon/M13343.java
+++ src/backjoon/M13343.java
@@ -1,35 +1,33 @@
 package backjoon;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class M13343 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] str = br.readLine().split(" ");
-        int n = Integer.parseInt(str[0]);
-        int m = Integer.parseInt(str[1]);
+        long n = Long.parseLong(str[0]);
+        long m = Long.parseLong(str[1]);
 
 
         bw.write(GCD(n,m)?"win" :"lose");
         br.close();
         bw.close();
     }
-    public static boolean GCD(int n, int m){
-        int min = Math.min(n, m);
-        int max = Math.max(n, m);
+    public static boolean GCD(long n, long m) {
+        long min = Math.min(n, m);
+        long max = Math.max(n, m);
 
-        // 3,3과 같이 선공이 바로 0으로 만들어 이길 수 있음
-        if(max % min == 0) return true;
-        
-        // 유클리드 호제법, 최소 공배수 구하는 중
-        // 큰 수를 작은 수로 계속 뺌
-        if(max - min < max){
-            return !GCD(min,max-min);
+        // 선공이 바로 승리할 수 있는 경우
+        if (max % min == 0) return true;
+
+        // 큰 수가 작은 수보다 많이 크면, 선공이 승리할 수 있음
+        if (max / min > 1) {
+            return true;
         }
-        
-        // 나머지 조건은 모두 선공이 이김
-        return true;
+
+        // 그 외의 경우, 후공이 다음 턴에서 승리할지 판단 (재귀)
+        return !GCD(min, max % min);
     }
 }
